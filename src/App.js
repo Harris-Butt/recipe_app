5b33850017444838c2d6828c8cff8a68dd6b5b0b
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2020-06-15 04:12:55 +0500

    Add Recipe Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useContext , useState} from 'react';
 import './App.css';
+import Recipe from './Recipe';
+
 
 function App() {
+  const APP_ID = 'd91497a3';
+  const APP_KEY = '3030df2fa63327cb2cba0644d4cef15b';
+
+  const[recipes,setRecipes] = useState([]);
+  const[search,setSearch] = useState("");
+  const [query,setQuery] = useState("chicken")
+
+  useEffect(() => {
+    getRecipes();
+  },[query] );  
+  const getRecipes = async()=>{
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`,{mode:'cors'});
+    const data = await response.json();
+    setRecipes(data.hits); 
+    console.log(data);
+  }
+  const updateSearch = e =>{
+    setSearch(e.target.value);
+  } 
+  const getSearch = e =>{
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="serach-form" onSubmit={getSearch}>
+        <input type="text"  className="serach-bar" value={search} onChange={updateSearch}/>
+        <button className="search-button" type="submit" >Search</button>
+      </form>
+      {recipes.map(recipe=>(
+        <Recipe 
+          key = {recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image} />
+      ))} 
     </div>
   );
 }
